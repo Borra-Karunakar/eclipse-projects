b3fa8c07b3f449311921bd29c61a28363e0dd105
Borra-Karunakar <137256445+Borra-Karunakar@users.noreply.github.com>
2025-07-31 09:57:09 +0530

    Add files via upload

---
+++ JUnit5FactorialExample/src/main/java/com/karunakar/math/FactorialUtil.java
@@ -0,0 +1,15 @@
+package com.karunakar.math;
+
+public class FactorialUtil {
+
+    public static int factorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Negative numbers not allowed");
+        }
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
