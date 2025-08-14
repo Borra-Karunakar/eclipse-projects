35183f1a107abe55ce6a2590ee84101b4c5d87af
Borra-Karunakar <137256445+Borra-Karunakar@users.noreply.github.com>
2025-08-14 09:47:58 +0530

    Add files via upload

---
+++ delivery-mock/src/main/java/com/example/deliverymock/DeliveryMockApplication.java
@@ -0,0 +1,11 @@
+package com.example.deliverymock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryMockApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryMockApplication.class, args);
+    }
+}
